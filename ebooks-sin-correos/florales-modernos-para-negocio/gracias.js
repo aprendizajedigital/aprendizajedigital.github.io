ef9b3d6a0cae2e60e402ee22756e2c628b79c7c6
Aprendizaje Digital <aprendizajedigitalparatodos@gmail.com>
2022-05-04 14:27:55 -0500

    04-05-22 | test animaciones responsivas en la pagina gracias

--- ebooks-sin-correos/florales-modernos-para-negocio/gracias.js
+++ ebooks-sin-correos/florales-modernos-para-negocio/gracias.js
@@ -1,34 +1,53 @@
-//animate__fadeInLeft
-const animate__fadeInLeft = new IntersectionObserver(entries => {
+// //animate__fadeInLeft
+// const animate__fadeInLeft = new IntersectionObserver(entries => {
+//   // Loop over the entries
+//   entries.forEach(entry => {
+//     // If the element is visible
+//     if (entry.isIntersecting) {
+//       // Add the animation class
+//       entry.target.classList.add('animate__animated');
+//       entry.target.classList.add('animate__fadeInLeft');
+//     }
+//   });
+// });
+
+// animate__fadeInLeft.observe(document.querySelector('.main__title'));
+// animate__fadeInLeft.observe(document.querySelector('.main__description'));
+
+// //animate__fadeInRight
+// const animate__fadeInRight = new IntersectionObserver(entries => {
+//   // Loop over the entries
+//   entries.forEach(entry => {
+//     // If the element is visible
+//     if (entry.isIntersecting) {
+//       // Add the animation class
+//       entry.target.classList.add('animate__animated');
+//       entry.target.classList.add('animate__fadeInRight');
+//     }
+//   });
+// });
+
+// animate__fadeInRight.observe(document.querySelector('.main__subtitle'));
+// animate__fadeInRight.observe(document.querySelector('.main__icon'));
+
+//animate__fadeIn
+const animate__fadeIn = new IntersectionObserver(entries => {
   // Loop over the entries
   entries.forEach(entry => {
     // If the element is visible
     if (entry.isIntersecting) {
       // Add the animation class
       entry.target.classList.add('animate__animated');
-      entry.target.classList.add('animate__fadeInLeft');
+      entry.target.classList.add('animate__fadeIn');
     }
   });
 });
 
-animate__fadeInLeft.observe(document.querySelector('.main__title'));
-animate__fadeInLeft.observe(document.querySelector('.main__description'));
-
-//animate__fadeInRight
-const animate__fadeInRight = new IntersectionObserver(entries => {
-  // Loop over the entries
-  entries.forEach(entry => {
-    // If the element is visible
-    if (entry.isIntersecting) {
-      // Add the animation class
-      entry.target.classList.add('animate__animated');
-      entry.target.classList.add('animate__fadeInRight');
-    }
-  });
-});
-
-animate__fadeInRight.observe(document.querySelector('.main__subtitle'));
-animate__fadeInRight.observe(document.querySelector('.main__icon'));
+animate__fadeIn.observe(document.querySelector('.main__title'));
+animate__fadeIn.observe(document.querySelector('.main__subtitle'));
+animate__fadeIn.observe(document.querySelector('.main__description'));
+animate__fadeIn.observe(document.querySelector('.main__icon'));
+animate__fadeIn.observe(document.querySelector('.main__button'));
 
 //DESCARGAR EBOOK
 setTimeout(function () {
