8c5019087a02a9d062676c97b79ed1ef21d105d8
Aprendizaje Digital <aprendizajedigitalparatodos@gmail.com>
2022-05-31 04:28:44 -0500

    31-05-22 | progreso de la pagina ebook regalo de cuyes como negocio

---
+++ ebooks-sin-correos/el-negocio-de-la-crianza-tecnificada-de-cuyes/ebook.js
@@ -0,0 +1,98 @@
+
+// //animate__bounceIn
+// const animate__bounceIn = new IntersectionObserver(entries => {
+//   // Loop over the entries
+//   entries.forEach(entry => {
+//     // If the element is visible
+//     if (entry.isIntersecting) {
+//       // Add the animation class
+//       entry.target.classList.add('animate__animated');
+//       entry.target.classList.add('animate__bounceIn');
+//     }
+//   });
+// });
+
+// animate__bounceIn.observe(document.querySelector('.form__button')); 
+
+//   //animate__fadeInLeft
+//   const animate__fadeInLeft = new IntersectionObserver(entries => {
+//     // Loop over the entries
+//     entries.forEach(entry => {
+//       // If the element is visible
+//       if (entry.isIntersecting) {
+//         // Add the animation class
+//         entry.target.classList.add('animate__animated');
+//         entry.target.classList.add('animate__fadeInLeft');
+//       }
+//     });
+//   });
+
+//   animate__fadeInLeft.observe(document.querySelector('.container-ebook')); 
+
+//   //animate__fadeInRight
+//   const animate__fadeInRight = new IntersectionObserver(entries => {
+//     // Loop over the entries
+//     entries.forEach(entry => {
+//       // If the element is visible
+//       if (entry.isIntersecting) {
+//         // Add the animation class
+//         entry.target.classList.add('animate__animated');
+//         entry.target.classList.add('animate__fadeInRight');
+//       }
+//     });
+//   });
+
+//   animate__fadeInRight.observe(document.querySelector('.form-container'));
+
+//animate__zoomIn
+const animate__zoomIn = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('animate__animated');
+      entry.target.classList.add('animate__zoomIn');
+    }
+  });
+});
+
+animate__zoomIn.observe(document.querySelector('.container-ebook'));
+animate__zoomIn.observe(document.querySelector('.form-container'));
+animate__zoomIn.observe(document.querySelector('.form__button'));
+
+//-------------------------------------------Sweet Alert en Accion-------------------------------------------
+
+document.querySelector(".form__button").addEventListener('click', function () {
+  window.location.href = './gracias.html';
+});
+
+// document.querySelector(".form__button").addEventListener('click', function () {
+
+//   let timerInterval;
+//   if (true) {
+//     Swal.fire({
+//       title: 'Descargando Ebook...',
+//       html: 'Por favor espere <strong></strong> segundos...',
+//       imageUrl: './ebooks-sin-correos/florales-modernos-para-negocio/buscando-ebook.jpg',
+//       imageWidth: 400,
+//       timer: 4000,
+//       timerProgressBar: true,
+//       allowEscapeKey: false,
+//       allowOutsideClick: false,
+//       didOpen: () => {
+//         Swal.showLoading();
+//         const timer_strong = Swal.getHtmlContainer().querySelector('strong')
+//         timerInterval = setInterval(() => {
+//           timer_strong.textContent = (Swal.getTimerLeft() / 1000).toFixed(0);
+//         }, 100);
+//       },
+//       willClose: () => {
+//         clearInterval(timerInterval);
+//         window.location.href = './gracias.html';
+//       }
+//     });
+//     // document.querySelector(".form__button").setAttribute("disabled", "disabled");
+//     // document.querySelector(".form__button").style.display = "none";
+//   }
+// });
---
+++ ebooks-sin-correos/el-negocio-de-la-crianza-tecnificada-de-cuyes/gracias.js
@@ -0,0 +1,96 @@
+// //animate__fadeInLeft
+// const animate__fadeInLeft = new IntersectionObserver(entries => {
+//   // Loop over the entries
+//   entries.forEach(entry => {
+//     // If the element is visible
+//     if (entry.isIntersecting) {
+//       // Add the animation class
+//       entry.target.classList.add('animate__animated');
+//       entry.target.classList.add('animate__fadeInLeft');
+//     }
+//   });
+// });
+
+// animate__fadeInLeft.observe(document.querySelector('.main__title'));
+// animate__fadeInLeft.observe(document.querySelector('.main__description'));
+
+// //animate__fadeInRight
+// const animate__fadeInRight = new IntersectionObserver(entries => {
+//   // Loop over the entries
+//   entries.forEach(entry => {
+//     // If the element is visible
+//     if (entry.isIntersecting) {
+//       // Add the animation class
+//       entry.target.classList.add('animate__animated');
+//       entry.target.classList.add('animate__fadeInRight');
+//     }
+//   });
+// });
+
+// animate__fadeInRight.observe(document.querySelector('.main__subtitle'));
+// animate__fadeInRight.observe(document.querySelector('.main__icon'));
+
+//animate__zoomIn
+const animate__zoomIn = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('animate__animated');
+      entry.target.classList.add('animate__zoomIn');
+    }
+  });
+});
+
+animate__zoomIn.observe(document.querySelector('.main__title'));
+animate__zoomIn.observe(document.querySelector('.main__subtitle'));
+animate__zoomIn.observe(document.querySelector('.main__description'));
+animate__zoomIn.observe(document.querySelector('.main__icon'));
+// animate__zoomIn.observe(document.querySelector('.main__button'));
+
+//DESCARGAR EBOOK
+setTimeout(function () {
+  // alert("5 SEGUNDOS");
+
+  // DownloadFile("gatillos-mentales.pdf");
+
+  // function DownloadFile(fileName) {
+  //   //Set the File URL.
+  //   var url = "./ebooks-sin-correos/florales-modernos-para-negocio/" + fileName;
+
+  //   //Create XMLHTTP Request.
+  //   var req = new XMLHttpRequest();
+  //   req.open("GET", url, true);
+  //   req.responseType = "blob";
+  //   req.onload = function () {
+  //     //Convert the Byte Data to BLOB object.
+  //     var blob = new Blob([req.response], { type: "application/octetstream" });
+
+  //     //Check the Browser type and download the File.
+  //     var isIE = false || !!document.documentMode;
+  //     if (isIE) {
+  //       window.navigator.msSaveBlob(blob, fileName);
+  //     } else {
+  //       var url = window.URL || window.webkitURL;
+  //       link = url.createObjectURL(blob);
+  //       var a = document.createElement("a");
+  //       a.setAttribute("download", fileName);
+  //       a.setAttribute("href", link);
+  //       document.body.appendChild(a);
+  //       a.click();
+  //       document.body.removeChild(a);
+  //     }
+  //   };
+  //   req.send();
+  // };
+
+  var link = document.createElement('a');
+
+  link.href = './ebooks-sin-correos/florales-modernos-para-negocio/los-10-secretos-que-las-floristas-NO-quieren-que-sepas.pdf';
+
+  link.download = 'los-10-secretos-que-las-floristas-NO-quieren-que-sepas.pdf';
+
+  link.dispatchEvent(new MouseEvent('click'));
+
+}, 10000);
