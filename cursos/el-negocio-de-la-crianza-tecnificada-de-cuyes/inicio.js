dbe41e0cafa81e6e9de3bc039f496d8c3696b1d4
Aprendizaje Digital <aprendizajedigitalparatodos@gmail.com>
2022-07-04 15:59:18 -0500

    clock update

--- cursos/el-negocio-de-la-crianza-tecnificada-de-cuyes/inicio.js
+++ cursos/el-negocio-de-la-crianza-tecnificada-de-cuyes/inicio.js
@@ -392,7 +392,7 @@ question.forEach(question => {
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy,
-      dayMonth = "07/03/",
+      dayMonth = "07/05/",
       birthday = dayMonth + yyyy;
 
     today = mm + "/" + dd + "/" + yyyy;
