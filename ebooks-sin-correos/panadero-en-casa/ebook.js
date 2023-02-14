0057e7357341114e6be4465ab8ed2f7a5e8fa9ea
Aprendizaje Digital <aprendizajedigitalparatodos@gmail.com>
2023-02-14 01:09:23 -0500

    cambio

--- ebooks-sin-correos/panadero-en-casa/ebook.js
+++ ebooks-sin-correos/panadero-en-casa/ebook.js
@@ -1,3 +1,12 @@
+function sleep(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+async function delay() {
+  console.log('Sleeping…');
+  await sleep(5000);
+  console.log('Awake…');
+}
 
 // //animate__bounceIn
 // const animate__bounceIn = new IntersectionObserver(entries => {
@@ -61,12 +70,51 @@ animate__zoomIn.observe(document.querySelector('.container-ebook'));
 animate__zoomIn.observe(document.querySelector('.form-container'));
 animate__zoomIn.observe(document.querySelector('.form__button'));
 
-// -- INICIO ENVIAR CORREO CON SMTPJS.COM
+// -- INICIO ENVIAR CORREO CON EMAIL.JS
 
-let btn = document.querySelector(".form__button").firstChild
+let btn = document.querySelector(".form__button").firstChild;
+let first_name = document.querySelector("#name").value;
+let email = document.querySelector("#email").value;
 
+let valid_first_name = document.querySelector(".valid-first-name");
+let invalid_first_name = document.querySelector(".invalid-first-name");
+
+let valid_email = document.querySelector(".valid-email");
+let invalid_email = document.querySelector(".invalid-email");
+
+//FUNCION QUE VALIDA LOS INPUT NOMBRE Y EMAIL
+function form_validate(param_first_name, param_email){
+  if(!(/^(?!\s*$)[a-zA-ZÑñÁáÉéÍíÓóÚúÜüÇç\s]{2,50}$/.test(param_first_name))){
+    invalid_first_name.style.display = "block";
+    valid_first_name.style.display = "none";
+  }else{
+    valid_first_name.style.display = "block";
+    invalid_first_name.style.display = "none";
+  }
+   
+  if(!(/^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/.test(param_email))){
+    invalid_email.style.display = "block";
+    valid_email.style.display = "none";
+  }else{
+    valid_email.style.display = "block";
+    invalid_email.style.display = "none";
+  }
+}
+
+// VALIDAR CUANDO PRESIONAMOS EL BOTON ENVIAR
 document.querySelector("#form").addEventListener('submit', function (e) {
   e.preventDefault();
+
+  first_name = first_name.trim();
+  email = email.trim();
+
+  //--INICIO VALIDANDO FORMULARIO--
+    form_validate(first_name, email);
+
+    if(invalid_first_name.style.display == "block" || invalid_email.style.display == "block")
+    return false;
+  //--FIN VALIDANDO FORMULARIO--
+
   btn.nodeValue = "Enviando..."
 
   const serviceID = 'default_service';
@@ -74,14 +122,23 @@ document.querySelector("#form").addEventListener('submit', function (e) {
 
   emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.nodeValue = 'Enviado';
+      btn.nodeValue = 'Listo!';
       // alert('Tu ebook fue enviado');
     }, (err) => {
       // btn.nodeValue = 'Enviado';
       // alert(JSON.stringify(err));
     });
+
+  delay();
+
+  window.location.href = './gracias-aqui-tienes-tu-libro-digital-de-cuyes.html';
 });
-// -- FIN ENVIAR CORREO CON SMTPJS.COM
+
+// VALIDAR CUANDO PRESIONAMOS UNA TECLA
+window.addEventListener("keyup", function (e){
+  form_validate(first_name, email);
+});
+// -- FIN ENVIAR CORREO CON EMAIL.JS
 
 //-------------------------------------------Sweet Alert en Accion-------------------------------------------
 
