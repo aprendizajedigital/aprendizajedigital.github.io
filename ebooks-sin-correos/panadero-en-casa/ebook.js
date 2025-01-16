4c88a45226deeb2035a8927217bdbb5231bc7764
Aprendizaje Digital <aprendizajedigitalparatodos@gmail.com>
2025-01-16 04:21:03 -0500

    hola

--- ebooks-sin-correos/panadero-en-casa/ebook.js
+++ ebooks-sin-correos/panadero-en-casa/ebook.js
@@ -60,7 +60,7 @@ document.querySelector("#form").addEventListener('submit', function (e) {
 
   //INICIO EMAIL.JS
   const serviceID = 'default_service';
-  const templateID = 'template_fc02x9n';  
+  const templateID = 'template_yys5zjp';  
 
   emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
