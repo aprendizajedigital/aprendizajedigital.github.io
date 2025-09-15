6d99309c913e6d355115ad50acfdee474e636052
Aprendizaje Digital <aprendizajedigitalparatodos@gmail.com>
2025-09-15 07:37:56 -0500

    cambios

---
+++ cursos/el-negocio-de-la-crianza-tecnificada-de-cuyes/_loader-page.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+    document.querySelector("body").style.overflow = "auto";
+    document.querySelector(".o-loader-page").style.visibility = "hidden";
+    document.querySelector(".o-loader-page").style.opacity = "0";
+    }, 0.1);
+});
