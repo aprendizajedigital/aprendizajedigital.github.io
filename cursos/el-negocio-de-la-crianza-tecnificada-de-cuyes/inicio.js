d3140fe0a873951f59535adce938bd05de3cf19a
Aprendizaje Digital <aprendizajedigitalparatodos@gmail.com>
2025-09-15 07:48:36 -0500

    hola

--- cursos/el-negocio-de-la-crianza-tecnificada-de-cuyes/inicio.js
+++ cursos/el-negocio-de-la-crianza-tecnificada-de-cuyes/inicio.js
@@ -160,7 +160,7 @@ let user131 = { name: "Aldo", photo: "131.jpg", country: "Brasil" }
 let users = [user01, user02, user03, user04, user05, user06, user07, user08, user09, user10, user10, user10, user10, user11, user12, user13, user14, user15, user16, user17, user18, user19, user20, user21, user22, user23, user24, user25, user26, user27, user28, user29, user30, user31, user32, user33, user34, user35, user36, user37, user38, user39, user40, user41, user42, user43, user44, user45, user46, user47, user48, user49, user50, user51, user52, user53, user54, user55, user56, user57, user58, user59, user60, user61, user62, user63, user64, user65, user66, user67, user68, user69, user70, user71, user72, user73, user74, user75, user76, user77, user78, user79, user80, user81, user82, user83, user84, user85, user86, user87, user88, user89, user90, user91, user92, user93, user94, user95, user96, user97, user98, user99, user100, user101, user102, user103, user104, user105, user106, user107, user108, user109, user110, user111, user112, user113, user114, user115, user116, user117, user118, user119, user120]
 
 //Enlace de afiliado
-const enlace_afiliado = function () {window.open("https://hotm.art/el-negocio-de-la-crianza-tecnificada-de-cuyes")}
+const enlace_afiliado = function () {window.open("https://hotm.art/oferta-por-lanzamiento-de-cuyes")} //https://hotm.art/el-negocio-de-la-crianza-tecnificada-de-cuyes
 
 const enlace_whatsapp = function () {window.open("https://bit.ly/contactar-con-un-asesor-personal")}
 
